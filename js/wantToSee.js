5e0ec0eff87b52c35d013fddb14f69987310d84d
Alexander Kashev <alexander@kashev.me>
2022-11-28 11:08:10 +0100

    Change to talk code indexing

--- js/wantToSee.js
+++ js/wantToSee.js
@@ -1,11 +1,15 @@
 let wantToSee = new Set();
 let randomSeed = Math.floor(Math.random() * 1000000);
 
+const checkboxes = Array.from(document.querySelectorAll(".want-to-see input"));
+
+const validCodes = checkboxes.map(el => el.dataset.code);
+
 function loadLocal() {
   if (localStorage.getItem("wantToSee")) {
     try {
       let maybeArr = JSON.parse(localStorage.getItem("wantToSee"));
-      if (Array.isArray(maybeArr)) {
+      if (Array.isArray(maybeArr) && maybeArr.every(code => validCodes.includes(code))) {
         wantToSee = new Set(maybeArr);
       }
     } catch(err) {
@@ -30,10 +34,8 @@ document.getElementById("sessions-header").setAttribute(
   document.getElementById("sessions-header").getAttribute("colspan") + 1
 );
 
-const checkboxes = Array.from(document.querySelectorAll(".want-to-see input"));
-
 checkboxes.forEach(el => {
-  if (wantToSee.has(el.dataset.index)) {
+  if (wantToSee.has(el.dataset.code)) {
     el.checked = true;
   }
 });
@@ -45,7 +47,7 @@ function calculateSessions() {
 
   let solutions = wants.reduce((acc, want) => 
     timeslots.filter(
-      slot => slot.dataset.index == want
+      slot => slot.dataset.code == want
     ).map(
       slot => slot.dataset.timeslot
     ).map(
@@ -66,7 +68,7 @@ function calculateSessions() {
     );
   } else {
     const solution = solutions[randomSeed % solutions.length];
-    timeslots.filter(el => solution[el.dataset.index]?.includes(el.dataset.timeslot)).forEach(el => {
+    timeslots.filter(el => solution[el.dataset.code]?.includes(el.dataset.timeslot)).forEach(el => {
       el.classList.add("must-see");
       document.querySelector(`[data-timeslot="${el.dataset.timeslot}"][data-station="${el.dataset.station}"]`).classList.add("must-see")
     });
@@ -78,12 +80,12 @@ function calculateSessions() {
 function wantChange() {
   // this - checkbox that changed
   const isChecked = this.checked;
-  const index = this.dataset.index;
+  const code = this.dataset.code;
 
   if (isChecked) {
-    wantToSee.add(index);
+    wantToSee.add(code);
   } else {
-    wantToSee.delete(index);
+    wantToSee.delete(code);
   }
 
   saveLocal();
